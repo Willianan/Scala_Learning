4bdab43e9ccaef73457aa39e5e8372d65ad10961
CharlesVan <williananjhon@hotmail.com>
2019-04-17 20:45:34 +0800

    Add files via upload

---
+++ Chapter07/scalaPackage/xh/Tiger.scala
@@ -0,0 +1,13 @@
+/**
+	* Auther:Charles·Van
+	* E-mail:williananjhon@hotmail.com
+	* Date:2019/3/30 9:11
+	* Project:ScalaLearning
+	* FileName:Tiger.scala
+	* Version 1.0
+	*/
+package Chapter7.ScalaPackage.xh
+
+class Tiger {
+
+}
