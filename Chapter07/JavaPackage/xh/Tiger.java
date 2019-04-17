87e8c192f565693d02ddedbca5a5ec4fa93be50b
CharlesVan <williananjhon@hotmail.com>
2019-04-17 20:42:31 +0800

    Add files via upload

---
+++ Chapter07/JavaPackage/xh/Tiger.java
@@ -0,0 +1,13 @@
+/**
+	* Auther:Charles·Van
+	* E-mail:williananjhon@hotmail.com
+	* Date:2019/3/29 16:27
+	* Project:ScalaLearning
+	* FileName:Tiger.java
+	* Version 1.0
+	*/
+package Chapter7.JavaPackage.xh;
+
+public class Tiger {
+
+}
