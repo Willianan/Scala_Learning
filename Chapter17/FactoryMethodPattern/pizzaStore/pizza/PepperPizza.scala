c0773ab8aa210739331e51c247bfe64c348ee888
CharlesVan <williananjhon@hotmail.com>
2019-05-04 14:37:18 +0800

    Add files via upload

---
+++ Chapter17/FactoryMethodPattern/pizzaStore/pizza/PepperPizza.scala
@@ -0,0 +1,16 @@
+/**
+	* Auther:Charles·Van
+	* E-mail:williananjhon@hotmail.com
+	* Date:2019/4/13 20:12
+	* Project:ScalaLearning
+	* FileName:PepperPizza.scala
+	* Version 1.0
+	*/
+package Chapter17.FactoryMethodPattern.pizzaStore.pizza
+
+class PepperPizza extends Pizza {
+	override def prepare(): Unit = {
+		this.name = "胡椒Pizza"
+		println(this.name + " preparing.....")
+	}
+}
---
+++ Chapter17/FactoryMethodPattern/pizzaStore/pizza/Pizza.scala
@@ -0,0 +1,25 @@
+/**
+	* Auther:Charles·Van
+	* E-mail:williananjhon@hotmail.com
+	* Date:2019/4/13 21:17
+	* Project:ScalaLearning
+	* FileName:Pizza.scala
+	* Version 1.0
+	*/
+package Chapter17.FactoryMethodPattern.pizzaStore.pizza
+
+abstract class Pizza {
+	var name:String = _
+	// 假定每种pizza的准备原材料不同，因此做成抽象的
+	def prepare() // 抽象方法
+
+	def cut(): Unit ={
+		println(this.name + " cutting....")
+	}
+	def bake(): Unit ={
+		println(this.name + " baking.....")
+	}
+	def box(): Unit ={
+		println(this.name + " boxing.....")
+	}
+}
