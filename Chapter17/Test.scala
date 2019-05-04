602a9473acdd2a3e586a6cf833fd9404e27f8393
CharlesVan <williananjhon@hotmail.com>
2019-05-04 15:01:12 +0800

    Add files via upload

---
+++ Chapter17/Test.scala
@@ -0,0 +1,13 @@
+/**
+	* Auther:Charles·Van
+	* E-mail:williananjhon@hotmail.com
+	* Date:2019/4/13 19:22
+	* Project:ScalaLearning
+	* FileName:Test.scala
+	* Version 1.0
+	*/
+package Chapter17
+
+class Test {
+// java.lang.Runtime
+}
